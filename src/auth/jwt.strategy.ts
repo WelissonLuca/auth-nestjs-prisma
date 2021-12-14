45704da5c0a017de88d6deff3eef5f182765d079
Welisson luca <welissonluca17@gmail.com>
2021-12-13 23:28:09 -0300

    feat: add jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { UserPayload } from './dto/user-payload.dto';
+import { UserFromJwt } from './dto/user-from-jwt.dto';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: UserPayload): Promise<UserFromJwt> {
+    return { id: payload.sub, email: payload.username };
+  }
+}
